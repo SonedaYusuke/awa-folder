3bb5488ee5bfe9ad0d350105dfc56068c3f17dc4
SonedaYusuke <yusuke.soneda@gmail.com>
2022-09-24 10:52:10 +0900

    scss入れる

--- components/Shabon.vue
+++ components/Shabon.vue
@@ -1,9 +1,16 @@
 <template>
-  <div>シャボン玉</div>
+  <div class="shabon"></div>
 </template>
 
 <script>
 export default {}
 </script>
 
-<style lang="scss" scoped></style>
+<style lang="scss" scoped>
+.shabon {
+  width: 80px;
+  height: 80px;
+  background-color: red;
+  border-radius: 100vh;
+}
+</style>
