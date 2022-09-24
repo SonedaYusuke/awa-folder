0851f50f6e25ad19ba18d3e2f2fea0fafac68c15
SonedaYusuke <yusuke.soneda@gmail.com>
2022-09-24 10:58:58 +0900

    シャボン玉コンポーネント

--- components/Shabon.vue
+++ components/Shabon.vue
@@ -1,16 +1,31 @@
 <template>
-  <div class="shabon"></div>
+  <div class="shabon">
+    <img class="shabon__image" :src="src" alt="" />
+  </div>
 </template>
 
 <script>
-export default {}
+export default {
+  props: {
+    src: {
+      type: String,
+      default: '',
+    },
+  },
+}
 </script>
 
 <style lang="scss" scoped>
 .shabon {
-  width: 80px;
-  height: 80px;
-  background-color: red;
+  width: 200px;
+  height: 200px;
   border-radius: 100vh;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  outline: 2px solid #000;
+  &__image {
+    width: 70%;
+  }
 }
 </style>
--- pages/index.vue
+++ pages/index.vue
@@ -1,6 +1,8 @@
 <template>
   <div>
-    <Shabon />
+    <Shabon
+      src="https://shanaiho-navi.jp/wp-content/uploads/2017/06/Fotolia_74570267_Subscription_Monthly_M-1024x682.jpg"
+    />
   </div>
 </template>
 
