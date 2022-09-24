d9ede178c3f839af085eed4fff341335244687b4
SonedaYusuke <yusuke.soneda@gmail.com>
2022-09-24 11:24:19 +0900

    ちょっと改良

--- pages/index.vue
+++ pages/index.vue
@@ -1,9 +1,12 @@
 <template>
   <div>
     <nuxt-link class="shabon-link" to="/チームシャボン">
-      <Shabon
-        src="https://shanaiho-navi.jp/wp-content/uploads/2017/06/Fotolia_74570267_Subscription_Monthly_M-1024x682.jpg"
-      />
+      <div class="shabon-wrapper">
+        <Shabon
+          src="https://shanaiho-navi.jp/wp-content/uploads/2017/06/Fotolia_74570267_Subscription_Monthly_M-1024x682.jpg"
+        />
+        <p class="team-name">チームシャボン</p>
+      </div>
     </nuxt-link>
   </div>
 </template>
@@ -13,8 +16,16 @@ export default {}
 </script>
 
 <style lang="scss" scoped>
+.shabon-wrapper {
+  width: fit-content;
+}
+
 .shabon-link {
   display: block;
   width: fit-content;
+}
+
+.team-name {
+  text-align: center;
 }
 </style>
