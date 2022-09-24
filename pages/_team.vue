3ddc57bcecc648dd5716aee2c655708c7a8e4355
SonedaYusuke <yusuke.soneda@gmail.com>
2022-09-24 11:12:28 +0900

    ルーティングできた

---
+++ pages/_team.vue
@@ -0,0 +1,18 @@
+<template>
+  <div>
+    <p>チーム名とか入りそう</p>
+    <span>{{ teamName }}</span>
+  </div>
+</template>
+
+<script>
+export default {
+  computed: {
+    teamName() {
+      return this.$route.params.team
+    },
+  },
+}
+</script>
+
+<style lang="scss" scoped></style>
--- pages/index.vue
+++ pages/index.vue
@@ -1,11 +1,20 @@
 <template>
   <div>
-    <Shabon
-      src="https://shanaiho-navi.jp/wp-content/uploads/2017/06/Fotolia_74570267_Subscription_Monthly_M-1024x682.jpg"
-    />
+    <nuxt-link class="shabon-link" to="/チームシャボン">
+      <Shabon
+        src="https://shanaiho-navi.jp/wp-content/uploads/2017/06/Fotolia_74570267_Subscription_Monthly_M-1024x682.jpg"
+      />
+    </nuxt-link>
   </div>
 </template>
 
 <script>
 export default {}
 </script>
+
+<style lang="scss" scoped>
+.shabon-link {
+  display: block;
+  width: fit-content;
+}
+</style>
